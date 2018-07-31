2c0adda6f9f8c2656079c5b06245b5e4501c404e
seogeekk <karezb@192-168-1-104.tpgi.com.au>
2018-07-31 22:48:29 +1000

    Add routing
    Deactivated for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+import { Routing } from './app.routing';
+import { AuthenticationService } from './_services/authentication.service';
+import { UserService } from './_services/user.service';
+import { AuthGuard } from './_guards/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, Routing, FormsModule, HttpClientModule
   ],
-  providers: [],
+  providers: [
+      {provide: APP_BASE_HREF, useValue : '/' },
+      AuthGuard,
+      AuthenticationService,
+      UserService
+      ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
